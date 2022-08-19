38f7b8ebd87b574b3a842cb57e5784b244f80da2
Pitamber26 <106551793+Pitamber26@users.noreply.github.com>
2022-08-19 13:29:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 document
   .querySelector("img.img1")
-  .setAttribute("src", `images/dice${randomNumber1}.png`);
+  .setAttribute("src", `dice${randomNumber1}.png`);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 document
   .querySelector("img.img2")
-  .setAttribute("src", `images/dice${randomNumber2}.png`);
+  .setAttribute("src", `dice${randomNumber2}.png`);
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
